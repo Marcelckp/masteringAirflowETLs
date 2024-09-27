17b20ecac064a6e2e88d9c507f900fb161eee35d
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2024-09-28 00:17:34 +0200

    chore: created ignore files file

--- script.sh
+++
@@ -1,5 +0,0 @@
-systemctl stop docker
-e2fsck -fy /dockerimages/ignite_vfs.ext4
-resize2fs /dockerimages/ignite_vfs.ext4 10G
-mount /dockerimages/ignite_vfs.ext4 /var/lib/docker
-systemctl start docker
